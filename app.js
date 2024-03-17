a8c0fa5cf09f072e8c9335386d14cee0bb55157a
Gupta24Divyanshu <divyanshugupta670@gmail.com>
2024-03-17 11:42:54 +0530

    episode-03

--- app.js
+++ app.js
@@ -1,57 +1,51 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-
-
-// const heading = React.createElement("h1", {id:"heading"}, "Hello World From React!");
-// here the object is used for providing attributes to a tag like their id ans so on
-// createElement - it creates javascript object
-
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-
-// root.render(heading);
-// this render will convert javascript object into h1 tag and put it into dom or root element
-
-
-
-/*
-for nested --- 
-   <div id:"parent">
-      <div id:"child1">
-       <h1>i am h1 tag</h1>
-       <h2>i am h2 tag</h2>
-       </div>
-
-       <div id:"child2">
-       <h1>i am h1 tag</h1>
-       <h2>i am h2 tag</h2>
-       </div>
-     </div>
- */      
-
-
-
-
-// const parent = React.createElement("div", {id:"parent"}, [
-//     React.createElement("div", {id:"child1"}, [
-//     React.createElement("h1", {}, "i am h1 tag"),
-//     React.createElement("h2", {}, "i am h2 tag")
-//     ]),
-//     React.createElement("div", {id:"child2"}, [
-//     React.createElement("h1", {}, "i am h1 tag"),
-//     React.createElement("h2", {}, "i am h2 tag")
-//     ])
-//     ]);
-
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-
-// root.render(parent);
-
-// ----------------------------- episode 3-------------------------------------------------------------
-
-// react.createElement => object => then it render on dom and it gives HTML element.
+// React.createElement => object => and when this object is render on the dom then it becomes html element
 
 const heading = React.createElement("h1", {id:"heading"}, "Namaste-react");
 
-const root = ReactDOM.createRoot(document.getElementById("root"));  // with the help of this we can show react element on browser
+const root = ReactDOM.createRoot(document.getElementById("root"));  // with the help of this(reactdom) we can show react element on browser
 root.render(heading);
+
+
+//----------------------------------------------------------------------------------------------
+
+// above syntax for creating react element is tough , it is not readable , not developer friendly .
+// JSX (javascript syntax) - through this we can easily create react element , it makes development and developer life easier.
+// JSX is not html is js , its just like that and it is separate and react is separate
+
+// jsx is not a pure javascript , before it reaches the browser or js it is transpiled by parcels(babel -- > package)
+// jsx ->babel transpiles it-> React.createElement -> js object -> when render on dom it gives html element.
+
+const jsxHeading = <h1 id="heading">Namaste React using jsx</h1>;
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(jsxHeading);
+
+
+//---------------React component----------------------------------------------------------------------
+
+// class based -- old way of writing code ( it uses javascript classes)
+// functional based -- new way of writing code ( a normal javascript functions that returns some piece of jsx code)
+
+const HeadingComponent = () => {
+    return <h1 className="heading">Nmaste React using functional component</h1>;
+};
+
+const HeadingComponent1 = () => <h1 className="heading">Namaste React using functional componenet 1</h1>
+const number = 10000;
+const HeadingComponent2 = () => (
+    <div id = "container">
+        <h2>{number}</h2>
+        <h2>{100+2000}</h2>
+        {heading}
+        {HeadingComponent()}
+        <HeadingComponent1 />         
+        <h1 className="heading">Namaste React using functional componenet2</h1>
+    </div>
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<HeadingComponent2 />);
+
